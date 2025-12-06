7dda8ed6cdef7f6d195a7ba9ee3522f67c159b41
Martin Webster <martin.webster@griffiths-waite.co.uk>
2025-12-06 13:34:24 +0000

    refactor day6

--- day6/day06.py
+++ day6/day06.py
@@ -16,38 +16,29 @@ def total_maths(lines: list):
 
     total = 0
     for sum in sums:
-        operators = sum[:-1]
-        sumTotal = int(operators[0])
-        if sum[-1] == "+":
-            for part in operators[1:]:
-                sumTotal = sumTotal + int(part)
-        else:
-            for part in operators[1:]:
-                sumTotal = sumTotal * int(part)
-        total = total + sumTotal
-    return total
-
-
-def part_2(lines: list, cols: list):
-
-    sums = [[line[col[0] : col[1]] for line in lines] for col in cols]
-
-    total = 0
-    for sum in sums:
-        operators = sum[:-1]
-        sumTotal = 0
-        for colIndex in range(len(sum[0])):
-            num = [operator[colIndex] for operator in operators]
-            num = int("".join(num))
-            if sum[-1][0] == "+":
-                sumTotal += num
+        operands = [int(x) for x in sum[:-1]]
+        operator = sum[-1]
+        sumTotal = operands[0]
+        for operand in operands[1:]:
+            if operator == "+":
+                sumTotal = sumTotal + operand
             else:
-                sumTotal = num if sumTotal == 0 else sumTotal * num
+                sumTotal = sumTotal * operand
         total = total + sumTotal
     return total
 
 
-def getCoumnDefs(operatorLine):
+def part_2(lines: list):
+
+    max_line_length = max(len(line) for line in lines)
+    operator_line = lines[-1].ljust(max_line_length)
+    col_definitions = _build_column_definitions(operator_line)
+
+    sums = [[line[col[0] : col[1]] for line in lines] for col in col_definitions]
+    return sum(_calculateColumnSum(sum) for sum in sums)
+
+
+def _build_column_definitions(operatorLine):
     columns = []
     start = 0
     for index, char in enumerate(operatorLine):
@@ -57,3 +48,16 @@ def getCoumnDefs(operatorLine):
                 start = index
     columns.append([start, len(operatorLine)])
     return columns
+
+
+def _calculateColumnSum(sum: list):
+    operands, operator = sum[:-1], sum[-1][0]
+    total = 0
+    for colIndex in range(len(sum[0])):
+        digits = [operand[colIndex] for operand in operands]
+        num = int("".join(digits))
+        if operator == "+":
+            total += num
+        else:
+            total = num if total == 0 else total * num
+    return total
--- day6/test_day06.py
+++ day6/test_day06.py
@@ -1,4 +1,4 @@
-from day6.day06 import getCoumnDefs, part_2, total_maths
+from day6.day06 import part_2, total_maths
 
 
 def test_example_part1():
@@ -15,19 +15,13 @@ def test_part1():
 
 def test_example_part2():
     data = readFile("day6/sample.txt").splitlines()
-    maxLength = max(len(line) for line in data)
-    data = [line.ljust(maxLength) for line in data]
-    cols = getCoumnDefs(data[3])
-
-    assert part_2(data, cols) == 3263827
+    assert part_2(data) == 3263827
 
 
 def test_part2():
     data = readFile("day6/input.txt").splitlines()
-    maxLength = max(len(line) for line in data)
-    data = [line.ljust(maxLength) for line in data]
-    cols = getCoumnDefs(data[4])
-    assert part_2(data, cols) == 8674740488592
+
+    assert part_2(data) == 8674740488592
 
 
 def readFile(path):
