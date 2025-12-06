0d26e79291d46af6e89f84bc170241fc5f0d5de8
Martin Webster <martin.webster@griffiths-waite.co.uk>
2025-12-06 12:39:58 +0000

    day6 refactor

--- day6/day06.py
+++ day6/day06.py
@@ -30,12 +30,7 @@ def total_maths(lines: list):
 
 def part_2(lines: list, cols: list):
 
-    sums = []
-    for col in cols:
-        sum = []
-        for line in lines:
-            sum.append(line[col[0] : col[1]])
-        sums.append(sum)
+    sums = [[line[col[0] : col[1]] for line in lines] for col in cols]
 
     total = 0
     for sum in sums:
@@ -45,27 +40,20 @@ def part_2(lines: list, cols: list):
             num = [operator[colIndex] for operator in operators]
             num = int("".join(num))
             if sum[-1][0] == "+":
-                sumTotal = sumTotal + num
+                sumTotal += num
             else:
-                if sumTotal == 0:
-                    sumTotal = num
-                else:
-                    sumTotal = sumTotal * num
+                sumTotal = num if sumTotal == 0 else sumTotal * num
         total = total + sumTotal
     return total
 
 
 def getCoumnDefs(operatorLine):
-    currentIndex = 0
     columns = []
-    for index in range(len(operatorLine)):
-        char = operatorLine[index]
-        if char == "+" or char == "*":
+    start = 0
+    for index, char in enumerate(operatorLine):
+        if char in "+*":
             if index > 0:
-                columns.append([currentIndex, index - 1])
-                currentIndex = index
-    columns.append([currentIndex, len(operatorLine)])
+                columns.append([start, index - 1])
+                start = index
+    columns.append([start, len(operatorLine)])
     return columns
-
-
-# 333210
